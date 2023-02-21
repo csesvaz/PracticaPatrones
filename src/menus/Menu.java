4b7745afd3a8a2855df68bcb6b47425c3ef76926
Csesvaz <camilocococo@gmail.com>
2023-02-21 22:01:58 +0100

    final del dia 21FEB23

--- src/menus/Menu.java
+++ src/menus/Menu.java
@@ -1,12 +1,65 @@
 package menus;
+
+import EntradaSalida.MyInput;
 import modelo.JuegosReunidos;
+import modelo.Jugable;
+
 public class Menu {
 	private JuegosReunidos juegos;
-	
-	//Constructor
+
+	// Constructor
 	public Menu(JuegosReunidos juegos) {
-		this.juegos=juegos;
+		this.juegos = juegos;
+	}
+
+	public void jugar(Jugable juego) {
+		String numeroIntento;
+		juego.reiniciaPartida();
+		juego.muestraNombre();
+		juego.muestraInfo();
+		do {
+			System.out.println("Introduce un numero entero de 1 a 10.");
+			numeroIntento = MyInput.readString();
+		} while (juego.juega(numeroIntento));
+	}
+
+	public void mostrarOpciones() {
+		System.out.println("¿A que quieres jugar?");
+		System.out.println("     1.Introduce 1 si quieres jugar a Adivinar un Número");
+		System.out.println("     2.Introduce 2 si quieres jugar a Adivinar un NúmeroPar");
+		System.out.println("     3.Introduce 3 si quieres jugar a Adivinar un NúmeroImpar");
+	}
+
+	public boolean chequearOpcion(int opcion) {
+		if (opcion == 1 || opcion == 2 || opcion == 3) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int eligeOpciones() {
+		int opcion;
+		boolean marca = true;
+		do {
+			mostrarOpciones();
+			opcion = MyInput.readInt();
+			if (chequearOpcion(opcion)) {
+				marca = false;
+			} else {
+				System.out.println("La opcion introducida no es correcta, inténtelo de nuevo.");
+			}
+		} while (marca);
+
+		return opcion;
 	}
 	
-	
+	public void ejecuta() {
+		String respuesta;
+		do {
+			jugar(juegos.recuperarJuego(eligeOpciones()));
+			System.out.println("¿Quieres seguir jugando?. En caso afirmativo introduzca \"s\"");
+			respuesta=MyInput.readString();
+		} while (respuesta.equals("s"));
+	}
 }
--- src/modelo/JuegosReunidos.java
+++ src/modelo/JuegosReunidos.java
@@ -1,7 +1,5 @@
 package modelo;
 
-import EntradaSalida.MyInput;
-
 public class JuegosReunidos {
 	// Array de Juegos
 	private Jugable[] juegos = new Jugable[3];
@@ -9,61 +7,15 @@ public class JuegosReunidos {
 	// constructors
 	public JuegosReunidos() {
 		juegos[0] = new JuegoAdivinaNumero(5, 7);
-		juegos[1] = new JuegoAdivinaNumeroPar(5, 3);
-		juegos[2] = new JuegoAdivinaNumeroImpar(5, 6);
+		juegos[1] = new JuegoAdivinaNumeroPar(5, 6);
+		juegos[2] = new JuegoAdivinaNumeroImpar(5, 3);
 	}
 
 	public Jugable recuperarJuego(int opcion) {
 		return juegos[opcion - 1];
 	}
 
-	public void jugar(Jugable juego) {
-		String numeroIntento;
-		juego.reiniciaPartida();
-		juego.muestraNombre();
-		juego.muestraInfo();
-		do {
-			System.out.println("Introduce un numero entero de 1 a 10.");
-			numeroIntento = MyInput.readString();
-		} while (juego.juega(numeroIntento));
-	}
-
-	public void mostrarOpciones() {
-		System.out.println("¿A que quieres jugar?");
-		System.out.println("     1.Introduce 1 si quieres jugar a Adivinar un Número");
-		System.out.println("     2.Introduce 2 si quieres jugar a Adivinar un NúmeroPar");
-		System.out.println("     3.Introduce 3 si quieres jugar a Adivinar un NúmeroImpar");
-	}
-
-	public boolean chequearOpcion(int opcion) {
-		if (opcion == 1 || opcion == 2 || opcion == 3) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	public int eligeOpcion() {
-		int opcion;
-		boolean marca = true;
-		do {
-			mostrarOpciones();
-			opcion = MyInput.readInt();
-			if (chequearOpcion(opcion)) {
-				marca = false;
-			} else {
-				System.out.println("La opcion introducida no es correcta, inténtelo de nuevo.");
-			}
-		} while (marca);
-
-		return opcion;
-	}
-
-	public void ejecuta() {
-		String respuesta;
-		do {
-			jugar(juegos.recuperarJuego(eligeOpcion()));
-			System.out.println("¿Quieres seguir jugando?. En caso afirmativo introduzca \"s\"");
-		} while (chequearOpcion(0));
-	}
+	
+	
+	
 }
